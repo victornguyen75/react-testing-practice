8aca5508a46e97e712778eedb5974c810ad0daf6
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-06-02 10:12:10 -0500

    style(Counter.js, Counter.css): add styling

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Counter.css";
 
 export default function Counter() {
-  return <h2 data-testid="header">This is a counter</h2>;
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState(1);
+
+  return (
+    <>
+      <h2 data-testid="header">This is a counter</h2>
+      <h3 data-testid="counter">{counter}</h3>
+      <button data-testid="subtract-button">-</button>
+      <input
+        data-testid="input"
+        type="number"
+        value={input}
+        className="center-text"
+      />
+      <button data-testid="add-button">+</button>
+    </>
+  );
 }
